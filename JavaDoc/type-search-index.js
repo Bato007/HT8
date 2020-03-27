6e28449d7f5415cf32b7da2a22884ddfb43ccb35
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-03-26 18:56:29 -0600

    JavaDoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Factory"},{"p":"<Unnamed>","l":"Heap"},{"p":"<Unnamed>","l":"JFCVectorHeap"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Patient"},{"p":"<Unnamed>","l":"Person"},{"p":"<Unnamed>","l":"VectorHeap"}]
